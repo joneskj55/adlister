70a2fb11d72bf0d5d387dc8b5dbb262c2687e062
Kevin Jones <joneskj55@gmail.com>
2017-01-30 15:28:41 -0600

    counter servlet second commit

--- src/main/java/CounterServlet.java
+++ src/main/java/CounterServlet.java
@@ -1,3 +1,4 @@
+import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -14,6 +15,6 @@ public class CounterServlet extends HttpServlet {
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
         counter += 1;
-        response.getWriter().println("<h1>The count is " + counter + " .</h1");
+        response.getWriter().println("<h1>The count is " + counter + ".</h1");
     }
 }
