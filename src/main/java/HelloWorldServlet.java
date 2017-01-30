4acaf8196abe2f950e4edd80d6da5b67385a1957
Kevin Jones <joneskj55@gmail.com>
2017-01-30 12:14:55 -0600

    Hello world servlet initial commit

--- src/main/java/HelloWorldServlet.java
+++ src/main/java/HelloWorldServlet.java
@@ -1,9 +1,25 @@
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
 
+// urlPatterns is a set of paths
+@WebServlet(name = "HelloWorldServlet", urlPatterns = {"/hello"})
 public class HelloWorldServlet extends HttpServlet {
-    protected void doGet(HttpServletRequest req, HttpServletResponse res) {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
 
+        response.setContentType("text/html"); //"text/html" = default
+
+        PrintWriter writer = response.getWriter();
+
+        String name = request.getParameter("name");
+        if (name != null) {
+            writer.println("<h1>Hello " + name + "</h1>"); //url = /hello?name=Kevin
+        } else {
+            writer.println("<h1>Hello, World!</h1>");
+        }
     }
 }
+
